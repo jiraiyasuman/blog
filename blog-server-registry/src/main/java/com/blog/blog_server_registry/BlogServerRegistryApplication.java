cf5bed7b08573a3da29deb18c85688d2d03ca9b8
jiraiyasuman <suman.talukdar53@gmail.com>
2026-03-10 10:28:13 +0530

    Update

--- blog-server-registry/src/main/java/com/blog/blog_server_registry/BlogServerRegistryApplication.java
+++ blog-server-registry/src/main/java/com/blog/blog_server_registry/BlogServerRegistryApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
+@EnableEurekaServer
 public class BlogServerRegistryApplication {
 
 	public static void main(String[] args) {
